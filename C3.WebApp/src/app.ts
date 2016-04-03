b8908dd7e6d610c3b9b2a62425d151c149e7e20f
Stephan Oehlert <stephan.oehlert@gmail.com>
2016-04-03 15:31:14 +0200

    Update title to C3.

--- C3.WebApp/src/app.ts
+++ C3.WebApp/src/app.ts
@@ -4,12 +4,9 @@ export class App {
   router: Router;
   
   configureRouter(config: RouterConfiguration, router: Router) {
-    config.title = 'Aurelia';
+    config.title = 'C3';
     config.map([
-      { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' },
-      { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' },
-      { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' },
-      { route: 'containers', name: 'Containers', moduleId: 'containers', nav: true, title: 'Containers'}
+      { route: ['', 'containers'], name: 'containers', moduleId: 'containers', nav: true, title: 'Containers'}
     ]);
 
     this.router = router;
