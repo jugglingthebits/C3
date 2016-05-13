0927946956ed02c663e89408d702cd993d5130c9
Stephan Oehlert <stephan.oehlert@gmail.com>
2016-05-13 17:11:55 +0200

    Make SystemNode transient.

--- C3.WebApp/src/main.ts
+++ C3.WebApp/src/main.ts
@@ -1,16 +1,19 @@
 import 'bootstrap';
 import {Aurelia} from 'aurelia-framework';
+import * as SystemNode from './system-context-diagram/system-node';
 
 export function configure(aurelia: Aurelia) {
-  aurelia.use
-    .standardConfiguration()
-    .developmentLogging();
+    aurelia.use
+        .standardConfiguration()
+        .developmentLogging();
 
-  //Uncomment the line below to enable animation.
-  //aurelia.use.plugin('aurelia-animator-css');
+    aurelia.container.registerTransient(SystemNode.SystemNode);
 
-  //Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
-  //aurelia.use.plugin('aurelia-html-import-template-loader')
+    //Uncomment the line below to enable animation.
+    //aurelia.use.plugin('aurelia-animator-css');
 
-  aurelia.start().then(() => aurelia.setRoot());
+    //Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
+    //aurelia.use.plugin('aurelia-html-import-template-loader')
+
+    aurelia.start().then(() => aurelia.setRoot());
 }
