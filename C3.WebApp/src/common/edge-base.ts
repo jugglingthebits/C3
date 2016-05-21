6aba2525a4d662dbd8b94be3bf3a4b06fca1ea58
Stephan Oehlert <stephan.oehlert@gmail.com>
2016-05-21 15:25:46 +0200

    Connect nodes at their edges.

--- C3.WebApp/src/common/edge-base.ts
+++ C3.WebApp/src/common/edge-base.ts
@@ -12,15 +12,70 @@ export abstract class EdgeBase {
 
     updatePath() {
         this.path = [];
+
+        if (this.areNodesOverlapping())
+            return;        
         
-        const sourceX = this.sourceNode.x + this.sourceNode.width / 2;
-        const sourceY = this.sourceNode.y + this.sourceNode.height / 2;
+        const sourcePoint = this.getNodeConnectionPoint(this.sourceNode, this.targetNode);
+        this.path.push(sourcePoint);
         
-        this.path.push(<Point>{x: sourceX, y: sourceY}); 
+        const targetPoint = this.getNodeConnectionPoint(this.targetNode, this.sourceNode);
+        this.path.push(targetPoint);
+    }
+    
+    private areNodesOverlapping(): boolean {
+        const overlapping = 
+               this.sourceNode.x + this.sourceNode.width > this.targetNode.x
+            && this.sourceNode.x < this.targetNode.x + this.targetNode.width
+            && this.sourceNode.y + this.sourceNode.height > this.targetNode.y
+            && this.sourceNode.y < this.targetNode.y + this.targetNode.height
+          
+        return overlapping;
+    }
+    
+    private getNodeConnectionPoint(connectionNode: NodeBase, otherNode: NodeBase) {
+        const connectionNodeCenter = this.getNodeCenter(connectionNode);
+        const otherNodeCenter = this.getNodeCenter(otherNode);
         
-        const targetX = this.targetNode.x + this.targetNode.width / 2;
-        const targetY = this.targetNode.y + this.targetNode.height / 2;
+        const diffX = Math.abs(connectionNodeCenter.x - otherNodeCenter.x);
+        const diffY = Math.abs(connectionNodeCenter.y - otherNodeCenter.y);
         
-        this.path.push(<Point>{x: targetX, y: targetY});
+        if (diffX < diffY) {
+            if (connectionNode.y > otherNode.y) {
+                const topCenter: Point = {
+                    x: connectionNode.x + connectionNode.width / 2,
+                    y: connectionNode.y
+                }
+                return topCenter;
+            }
+            else {
+                const bottomCenter: Point = {
+                    x: connectionNode.x + connectionNode.width / 2,
+                    y: connectionNode.y + connectionNode.height
+                }
+                return bottomCenter;
+            }
+        } else {
+            if (connectionNode.x > otherNode.x) {
+                const leftCenter: Point = {
+                    x: connectionNode.x,
+                    y: connectionNode.y + connectionNode.height / 2
+                }
+                return leftCenter;
+            }
+            else {
+                const bottomCenter: Point = {
+                    x: connectionNode.x + connectionNode.width,
+                    y: connectionNode.y + connectionNode.height / 2
+                }
+                return bottomCenter;
+            }
+        }
+    }
+
+    private getNodeCenter(node: NodeBase): Point {
+        const centerX = node.x + node.width / 2;
+        const centerY = node.y + node.height / 2;
+        return {x: centerX, y: centerY}
     }
 }
