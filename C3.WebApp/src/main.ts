8eabf092da57743c9ab9ca1d272c59fb1e406248
Stephan Oehlert <stephan.oehlert@gmail.com>
2016-05-22 13:19:42 +0200

    Use simpler import syntax.

--- C3.WebApp/src/main.ts
+++ C3.WebApp/src/main.ts
@@ -1,14 +1,16 @@
 import 'bootstrap';
 import {Aurelia} from 'aurelia-framework';
-import * as SystemNode from './system-context-diagram/system-node';
+import {SystemNode} from './system-context-diagram/system-node';
+import {StraightPathFinder} from './common/straight-path-finder';
+import {PathFinder} from './common/edge-base';
 
 export function configure(aurelia: Aurelia) {
     aurelia.use
         .standardConfiguration()
         .developmentLogging();
 
-    aurelia.container.registerTransient(SystemNode.SystemNode);
-
+    aurelia.container.registerTransient(SystemNode);
+    
     //Uncomment the line below to enable animation.
     //aurelia.use.plugin('aurelia-animator-css');
 
