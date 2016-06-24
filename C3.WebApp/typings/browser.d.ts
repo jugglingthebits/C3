8ff570a1bea4ae71113b9a03ee5a67b1a6c80cbe
Stephan Oehlert <stephan.oehlert@gmail.com>
2016-06-24 10:08:00 +0200

    Update typings to latest skeleton config.

--- C3.WebApp/typings/browser.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="browser/ambient/angular-protractor/index.d.ts" />
-/// <reference path="browser/ambient/es6-promise/index.d.ts" />
-/// <reference path="browser/ambient/hammerjs/index.d.ts" />
-/// <reference path="browser/ambient/jasmine/index.d.ts" />
-/// <reference path="browser/ambient/selenium-webdriver/index.d.ts" />
