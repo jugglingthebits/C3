448885cd4dbcc616c9329f88a779597d38dd2222
Stephan Oehlert <stephan.oehlert@gmail.com>
2016-08-17 11:24:49 +0200

    Add generated Aurelia 1.0 boilerplate.

--- C3.WebApp/src/app.ts
+++ C3.WebApp/src/app.ts
@@ -1,21 +1,3 @@
-import {Router, RouterConfiguration} from 'aurelia-router';
-
 export class App {
-    router: Router;
-    
-    configureRouter(config: RouterConfiguration, router: Router) {
-        config.title = 'C3';
-        config.map([
-            { route: ['', 'system-context-diagram/:id'], name: 'system-context-diagram', 
-              moduleId: 'system-context-diagram/system-context-diagram', title: 'System Context Diagram' },
-            { route: 'container-diagram/:id', name: 'container-diagram', 
-              moduleId: 'container-diagram/container-diagram', title: 'Container Diagram' },
-            { route: 'component-diagram/:id', name: 'component-diagram', 
-              moduleId: 'component-diagram/component-diagram', title: 'Component Diagram' },
-            { route: 'admin', name: 'admin', moduleId: 'admin/admin',
-              nav: true, title: 'Admin' }
-        ]);
-
-        this.router = router;
-    }
+  message = 'Hello World!';
 }
