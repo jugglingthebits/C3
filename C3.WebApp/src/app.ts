83496de2cf0df5be15330baee9a4c31b0eda2d16
jugglingthebits <stephan.oehlert@gmail.com>
2016-08-25 16:25:24 +0200

    Bring back app.ts and .html.

--- C3.WebApp/src/app.ts
+++ C3.WebApp/src/app.ts
@@ -1,3 +1,21 @@
+import {Router, RouterConfiguration} from 'aurelia-router';		
+		
 export class App {
-  message = 'Hello World!';
+    router: Router;
+    		
+    configureRouter(config: RouterConfiguration, router: Router) {		
+        config.title = 'C3';		
+        config.map([		
+            { route: ['', 'system-context-diagram/:id'], name: 'system-context-diagram', 		
+              moduleId: 'system-context-diagram/system-context-diagram', title: 'System Context Diagram' },		
+            { route: 'container-diagram/:id', name: 'container-diagram', 		
+              moduleId: 'container-diagram/container-diagram', title: 'Container Diagram' },		
+            { route: 'component-diagram/:id', name: 'component-diagram', 		
+              moduleId: 'component-diagram/component-diagram', title: 'Component Diagram' },		
+            { route: 'admin', name: 'admin', moduleId: 'admin/admin',		
+              nav: true, title: 'Admin' }		
+        ]);		
+		
+        this.router = router;		
+    }		
 }
