bc4f421fcc56c73855176c86efc498659eb0ef6e
jugglingthebits <stephan.oehlert@gmail.com>
2016-08-30 16:56:33 +0200

    Bring back old unit tests

--- C3.WebApp/test/unit/app.spec.ts
+++ C3.WebApp/test/unit/app.spec.ts
@@ -1,7 +1,39 @@
+import {Router, RouterConfiguration} from 'aurelia-router';
+
 import {App} from '../../src/app';
 
-describe('the app', () => {
-  it('says hello', () => {
-    expect(new App().message).toBe('Hello World!');
-  });
+class RouterStub {
+  routes;
+  
+  configure(handler) {
+    handler(this);
+  }
+  
+  map(routes) {
+    this.routes = routes;
+  }
+}	
+		
+describe('the App module', () => {		
+  var sut: App;
+  var mockedRouter: RouterStub;
+		
+  beforeEach(() => {
+    mockedRouter = new RouterStub();
+    sut = new App();
+    sut.configureRouter(mockedRouter, mockedRouter);
+  });	
+		
+  it('contains a router property', () => {		
+    expect(sut.router).toBeDefined();		
+  });		
+		
+  it('configures the router title', () => {		
+    expect(sut.router.title).toEqual('C3');		
+  });		
+		
+  it('should have a container-diagram route', () => {		
+    expect(sut.router.routes).toContain({ route: ['', 'system-context-diagram/:id'], name: 'system-context-diagram',
+                                          moduleId: 'system-context-diagram/system-context-diagram', title: 'System Context Diagram' });		
+  });		    
 });
