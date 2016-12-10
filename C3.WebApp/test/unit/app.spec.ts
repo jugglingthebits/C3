0ec142a79a65bdad1972bf4eb3f65f56f0b45dea
Stephan Oehlert <stephan.oehlert@gmail.com>
2016-12-10 15:00:59 +0100

    Copy over resources from a boilerplate aurelia project.

--- C3.WebApp/test/unit/app.spec.ts
+++ C3.WebApp/test/unit/app.spec.ts
@@ -1,35 +1,7 @@
 import {App} from '../../src/app';
 
-class RouterStub {
-    routes;
-
-    configure(handler) {
-        handler(this);
-    }
-
-    map(routes) {
-        this.routes = routes;
-    }
-}
-
-describe('the App module', () => {
-    var sut, mockedRouter;
-
-    beforeEach(() => {
-        mockedRouter = new RouterStub();
-        sut = new App();
-        sut.configureRouter(mockedRouter, mockedRouter);
-    });
-
-    it('contains a router property', () => {
-        expect(sut.router).toBeDefined();
-    });
-
-    it('configures the router title', () => {
-        expect(sut.router.title).toEqual('C3');
-    });
-
-    // it('should have a container-diagram route', () => {
-    //   expect(sut.router.routes).toContain({ route: ['','container-diagram'], name: 'container-diagram',  moduleId: 'container-diagram', nav: true, title:'Container Diagram' });
-    // });
+describe('the app', () => {
+  it('says hello', () => {
+    //expect(new App().message).toBe('Hello World!');
+  });
 });
